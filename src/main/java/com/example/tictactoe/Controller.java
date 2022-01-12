08bff1484623241719413ffb705c65866cc32075
Guralraz <87212802+Guralraz@users.noreply.github.com>
2022-01-12 17:56:21 +0100

    init

---
+++ src/main/java/com/example/tictactoe/Controller.java
@@ -0,0 +1,38 @@
+package com.example.tictactoe;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Button button6;
+
+    @FXML
+    private Button button7;
+
+    @FXML
+    private Button button8;
+
+    @FXML
+    private Button button9;
+
+    @FXML
+    private Button reset;
+
+}
